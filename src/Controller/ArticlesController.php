1099ef44da077f80c2b7dfd53978ebf440ea4ce8
FranckHuang <franck.huang2406@gmail.com>
2026-01-08 08:53:41 +0100

    first commit

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Articles;
+use App\Form\ArticlesType;
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/articles')]
+final class ArticlesController extends AbstractController
+{
+    #[Route(name: 'app_articles_index', methods: ['GET'])]
+    public function index(ArticlesRepository $articlesRepository): Response
+    {
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articlesRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_articles_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $article = new Articles();
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_articles_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('articles/new.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_articles_show', methods: ['GET'])]
+    public function show(Articles $article): Response
+    {
+        return $this->render('articles/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_articles_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ArticlesType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_articles_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('articles/edit.html.twig', [
+            'article' => $article,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_articles_delete', methods: ['POST'])]
+    public function delete(Request $request, Articles $article, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_articles_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Articles.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $return = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getReturn(): ?string
+    {
+        return $this->return;
+    }
+
+    public function setReturn(string $return): static
+    {
+        $this->return = $return;
+
+        return $this;
+    }
+}
